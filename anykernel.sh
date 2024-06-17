611cd5b53ad67906b421ba22bcc2ef2e3a394375
Developer From Android <catprojectsnew@gmail.com>
2024-06-17 18:42:53 -0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=CatKernel V1.7 5.15.149 [NotKangTime]
+kernel.string=CatKernel V1.8 5.15.152 [NotKangTime]
 do.devicecheck=1
 do.modules=0
 do.systemless=1
