fb2620ab34cbba4e8c18e953c0a9a2bed3b912ee
itzParsaYC <115403383+ParsaAslaniYC@users.noreply.github.com>
2024-07-15 22:31:10 +0330

    anykernel: Adapting the name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=6.1.96 | ImWolfYou 1.0 | GKI
+kernel.string=Yapin\' Kernel 1.0 5.15.151
 do.devicecheck=1
 do.modules=0
 do.systemless=1
